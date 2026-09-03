5ff9d344230b9dbdb1781507e5b7d7989ea73b59
yagoubyousifyagoub-HSE <yagoub.yousif.yagoub@gmail.com>
2026-09-03 13:28:26 +0300

    Update otp_verification_screen.dart

--- lib/features/auth/otp_verification_screen.dart
+++ lib/features/auth/otp_verification_screen.dart
@@ -62,8 +62,8 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
         MaterialPageRoute(builder: (_) => const DashboardScreen()),
         (route) => false,
       );
-    } catch (e) {
-      setState(() => _errorText = '${l10n.invalidOtpCode}\n$e');
+    } catch (_) {
+      setState(() => _errorText = l10n.invalidOtpCode);
     } finally {
       if (mounted) setState(() => _isVerifying = false);
     }
